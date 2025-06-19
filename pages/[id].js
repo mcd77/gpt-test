a7307a35e1cd5174923020e820d4d0293c405f63
mcd77 <126714281+mcd77@users.noreply.github.com>
2025-06-19 15:54:24 -0500

    Update [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -1,9 +1,6 @@
 import Head from 'next/head';
 import Layout from './components/layout';
 
-import Layout from '../components/layout';
-
-
 
 import { getAllPostIds, getPostData } from '../lib/posts';
 
