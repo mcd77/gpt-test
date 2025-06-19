bf6d09d6484515516e312110d3316ffac9da510b
mcd77 <126714281+mcd77@users.noreply.github.com>
2025-06-19 15:38:52 -0500

    fix module syntax error

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'gpt-test';
+
+module.exports = {
+  output: 'export',
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true,
+};
